982b6e3e2d33979b9cc68cc9ce560cb1829e8fad
Aaron Horler <aghorler@users.noreply.github.com>
2017-11-26 17:36:32 +1100

    Merge components to all

--- all.bat
+++ all.bat
@@ -3,8 +3,16 @@
 :: Privacy - Stop telemetry services.
 net stop DiagTrack
 net stop dmwappushservice
+net stop RemoteRegistry
+net stop RetailDemo
+net stop WinRM
+net stop WMPNetworkSvc
 
 :: Privacy - Delete telemetry services.
+sc config RemoteRegistry start=disabled
+sc config RetailDemo start=disabled
+sc config WinRM start=disabled
+sc config WMPNetworkSvc start=disabled
 sc delete DiagTrack
 sc delete dmwappushservice
 
@@ -582,3 +590,90 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Windows
 :: Privacy (optional) - Disable Microsoft account user authentication.
 :: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftAccount" /v "DisableUserAuth" /t REG_DWORD /d 1 /f
 :: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoConnectedUser" /t REG_DWORD /d 3 /f
+
+
+
+:: FIREWALL
+
+:: Privacy/Security - Firewall apps and services that may collect user data.
+:: Disable default rules.
+netsh advfirewall firewall set rule group="Connect" new enable=no
+netsh advfirewall firewall set rule group="Contact Support" new enable=no
+netsh advfirewall firewall set rule group="Cortana" new enable=no
+netsh advfirewall firewall set rule group="DiagTrack" new enable=no
+netsh advfirewall firewall set rule group="Feedback Hub" new enable=no
+netsh advfirewall firewall set rule group="Microsoft Photos" new enable=no
+netsh advfirewall firewall set rule group="OneNote" new enable=no
+netsh advfirewall firewall set rule group="Remote Assistance" new enable=no
+netsh advfirewall firewall set rule group="Windows Spotlight" new enable=no
+
+:: Delete custom rules in case script has previously run.
+netsh advfirewall firewall delete rule name="block_Connect_in"
+netsh advfirewall firewall delete rule name="block_Connect_out"
+netsh advfirewall firewall delete rule name="block_ContactSupport_in"
+netsh advfirewall firewall delete rule name="block_ContactSupport_out"
+netsh advfirewall firewall delete rule name="block_Cortana_in"
+netsh advfirewall firewall delete rule name="block_Cortana_out"
+netsh advfirewall firewall delete rule name="block_DiagTrack_in"
+netsh advfirewall firewall delete rule name="block_DiagTrack_out"
+netsh advfirewall firewall delete rule name="block_dmwappushservice_in"
+netsh advfirewall firewall delete rule name="block_dmwappushservice_out"
+netsh advfirewall firewall delete rule name="block_FeedbackHub_in"
+netsh advfirewall firewall delete rule name="block_FeedbackHub_out"
+netsh advfirewall firewall delete rule name="block_OneNote_in"
+netsh advfirewall firewall delete rule name="block_OneNote_out"
+netsh advfirewall firewall delete rule name="block_Photos_in"
+netsh advfirewall firewall delete rule name="block_Photos_out"
+netsh advfirewall firewall delete rule name="block_RemoteRegistry_in"
+netsh advfirewall firewall delete rule name="block_RemoteRegistry_out"
+netsh advfirewall firewall delete rule name="block_RetailDemo_in"
+netsh advfirewall firewall delete rule name="block_RetailDemo_out"
+netsh advfirewall firewall delete rule name="block_WinRM_in"
+netsh advfirewall firewall delete rule name="block_WinRM_out"
+netsh advfirewall firewall delete rule name="block_WMPNetworkSvc_in"
+netsh advfirewall firewall delete rule name="block_WMPNetworkSvc_out"
+netsh advfirewall firewall delete rule name="block_WSearch_in"
+netsh advfirewall firewall delete rule name="block_WSearch_out"
+
+:: Add custom blocking rules.
+netsh advfirewall firewall add rule name="block_Connect_in" dir=in program="%WINDIR%\SystemApps\Microsoft.PPIProjection_cw5n1h2txyewy\Receiver.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_Connect_out" dir=out program="%WINDIR%\SystemApps\Microsoft.PPIProjection_cw5n1h2txyewy\Receiver.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_ContactSupport_in" dir=in program="%WINDIR%\SystemApps\ContactSupport_cw5n1h2txyewy\ContactSupport.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_ContactSupport_out" dir=out program="%WINDIR%\SystemApps\ContactSupport_cw5n1h2txyewy\ContactSupport.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_Cortana_in" dir=in program="%WINDIR%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_Cortana_out" dir=out program="%WINDIR%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_DiagTrack_in" dir=in service="DiagTrack" action=block enable=yes
+netsh advfirewall firewall add rule name="block_DiagTrack_out" dir=out service="DiagTrack" action=block enable=yes
+netsh advfirewall firewall add rule name="block_dmwappushservice_in" dir=in service="dmwappushservice" action=block enable=yes
+netsh advfirewall firewall add rule name="block_dmwappushservice_out" dir=out service="dmwappushservice" action=block enable=yes
+netsh advfirewall firewall add rule name="block_FeedbackHub_in" dir=in program="%ProgramFiles%\WindowsApps\Microsoft.WindowsFeedbackHub_1.1708.2831.0_x64__8wekyb3d8bbwe\PilotshubApp.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_FeedbackHub_out" dir=out program="%ProgramFiles%\WindowsApps\Microsoft.WindowsFeedbackHub_1.1708.2831.0_x64__8wekyb3d8bbwe\PilotshubApp.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_OneNote_in" dir=in program="%ProgramFiles%\WindowsApps\Microsoft.Office.OneNote_17.8625.21151.0_x64__8wekyb3d8bbwe\onenoteim.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_OneNote_out" dir=out program="%ProgramFiles%\WindowsApps\Microsoft.Office.OneNote_17.8625.21151.0_x64__8wekyb3d8bbwe\onenoteim.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_Photos_in" dir=in program="%ProgramFiles%\WindowsApps\Microsoft.Windows.Photos_2017.39091.16340.0_x64__8wekyb3d8bbwe\Microsoft.Photos.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_Photos_out" dir=out program="%ProgramFiles%\WindowsApps\Microsoft.Windows.Photos_2017.39091.16340.0_x64__8wekyb3d8bbwe\Microsoft.Photos.exe" action=block enable=yes
+netsh advfirewall firewall add rule name="block_RemoteRegistry_in" dir=in service="RemoteRegistry" action=block enable=yes
+netsh advfirewall firewall add rule name="block_RemoteRegistry_out" dir=out service="RemoteRegistry" action=block enable=yes
+netsh advfirewall firewall add rule name="block_RetailDemo_in" dir=in service="RetailDemo" action=block enable=yes
+netsh advfirewall firewall add rule name="block_RetailDemo_out" dir=out service="RetailDemo" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WinRM_in" dir=in service="WinRM" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WinRM_out" dir=out service="WinRM" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WMPNetworkSvc_in" dir=in service="WMPNetworkSvc" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WMPNetworkSvc_out" dir=out service="WMPNetworkSvc" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WSearch_in" dir=in service="WSearch" action=block enable=yes
+netsh advfirewall firewall add rule name="block_WSearch_out" dir=out service="WSearch" action=block enable=yes
+
+
+
+:: MISCELLANEOUS
+
+:: Security - Do not hide extensions for know file types.
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt" /v "CheckedValue" /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
+
+:: Privacy - Do not show recently or frequently accessed files in Quick access (Explorer).
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowFrequent" /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowRecent" /t REG_DWORD /d 0 /f
+
+:: Privacy - Set NTP server to pool.ntp.org
+w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
