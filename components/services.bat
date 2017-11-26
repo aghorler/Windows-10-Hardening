5d392e2a5969adf26f3e40454f8dd2e2b5e916e6
Aaron Horler <aghorler@users.noreply.github.com>
2017-11-26 17:36:03 +1100

    Add additional services to disable

--- components/services.bat
+++ components/services.bat
@@ -3,7 +3,15 @@
 :: Privacy - Stop telemetry services.
 net stop DiagTrack
 net stop dmwappushservice
+net stop RemoteRegistry
+net stop RetailDemo
+net stop WinRM
+net stop WMPNetworkSvc
 
 :: Privacy - Delete telemetry services.
+sc config RemoteRegistry start=disabled
+sc config RetailDemo start=disabled
+sc config WinRM start=disabled
+sc config WMPNetworkSvc start=disabled
 sc delete DiagTrack
 sc delete dmwappushservice
